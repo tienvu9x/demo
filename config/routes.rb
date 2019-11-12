c07e2def2edcd420af3b4167590f17f4f044abf8
YOUR NAME <YOUR@EMAIL.com>
2019-11-12 21:01:42 +0700

    reset-password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 	resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
